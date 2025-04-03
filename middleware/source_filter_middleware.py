1d4aa616c511f98a45b7289fde8bbb32e9b843b4
caprixj <capricavoid7@gmail.com>
2025-04-04 01:58:46 +0300

    beta 1.16.3 (added: command usage log)

--- middleware/source_filter_middleware.py
+++ middleware/source_filter_middleware.py
@@ -3,6 +3,7 @@ from aiogram.enums import ContentType
 from aiogram.types import Message
 
 import resources.const.glob as glob
+from resources.funcs.funcs import get_current_datetime
 
 
 async def _is_accepted_content_type(event: Message) -> bool:
@@ -17,4 +18,6 @@ class SourceFilterMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
         if event.chat.type == "private" or event.chat.id == glob.rms.group_chat_id:
             if await _is_accepted_content_type(event):
+                print(f"{'private ' + str(event.chat.id) if event.chat.type == 'private' else 'group'} - "
+                      f"{get_current_datetime()}: {event.text}")
                 return await handler(event, data)
