ac901ed2bd5acc3ebe925e1d425cc05119a96047
caprixj <capricavoid7@gmail.com>
2025-05-26 23:56:15 +0300

    beta 1.31.1 (hotfix)

--- model/database/__init__.py
+++ model/database/__init__.py
@@ -3,4 +3,4 @@ from .awards import Award, AwardMember
 from .dynprices import Price, RateReset, PriceHistory
 from .jobs import Job, Employee, SalaryPayout
 from .member import Member, DelMember
-from .transactions import AddtTransaction, DeltTransaction, TpayTransaction\
+from .transactions import AddtTransaction, DeltTransaction, TpayTransaction
